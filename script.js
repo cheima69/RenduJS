031f29b4b9349ae637aa0175c30c150e16881bae
cheima69 <CHEIMA.ELAOUNI@latoile.em-lyon.com>
2025-12-16 11:13:49 +0100

    debut

---
+++ script.js
@@ -0,0 +1,69 @@
+// Afficher la navigation
+
+//     afficher le logo
+//     afficher les liens :
+//         Accueil
+//         Avantages
+//         Produits
+//         Services
+//         Témoignages
+
+
+
+
+
+// Afficher la section d’accueil
+//     afficher le titre principal
+//     afficher le texte de présentation
+//     afficher le bouton "Acheter"
+//     afficher le bouton "Explorer"
+
+
+
+
+// Afficher la section avantages
+//     afficher le titre ""
+
+//     pour chaque avantage
+//         afficher un bloc avec
+//             un titre
+//             une description
+
+
+
+
+
+
+// Afficher la section services
+//     afficher le titre "Vos sneakers, votre style"
+
+//     pour chaque service
+//         afficher un bloc avec
+//             un titre
+//             une description
+
+
+
+
+
+
+// Afficher la section témoignages
+//     afficher le titre "Ils parlent"
+
+//     pour chaque témoignage
+//         afficher
+//             la note
+//             le commentaire
+//             le nom du client
+
+
+
+
+
+
+
+
+// Afficher le pied de page
+//     afficher le nom de l’entreprise
+//     afficher les liens légaux
+//     afficher les réseaux sociaux
