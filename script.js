b6c5f8ed64d499aba73f9189aed364be93e941db
cheima69 <CHEIMA.ELAOUNI@latoile.em-lyon.com>
2025-12-16 17:24:39 +0100

    avancement

--- script.js
+++ script.js
@@ -1,12 +1,88 @@
 // Afficher la navigation
 
-//     afficher le logo
-//     afficher les liens :
-//         Accueil
-//         Avantages
-//         Produits
-//         Services
-//         Témoignages
+
+
+//     Récupérer  le logo
+//    Récupérer  les liens :
+
+
+
+
+
+fetch("https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/escape-game.json")
+    .then(response => response.json())
+    .then(data => {
+
+        const container = document.getElementById("container");
+        const menu = document.getElementById("menu");
+        const activite = document.getElementById("activite");
+        const titre = document.getElementById("titre-activite");
+        const texte = document.getElementById("texte-activite");
+        const reserverbtn = document.getElementById("btn-reserver");
+        const avantages = document.getElementById("avantages");
+        const liste = document.getElementById("listeAvantages");
+        const footer = document.getElementById("footer");
+
+        titre.textContent = data.nomCommercial;
+        texte.textContent = data.phraseAccroche;
+        reserverbtn.textContent = data.texteAppelAction;
+
+        data.avantagesClients.forEach(element => {
+            const para = document.createElement("p");
+            para.textContent = element;
+            liste.appendChild(para);
+        });
+
+        // Test pour services => 
+
+
+        const services = document.getElementById("liste-services");
+
+        data.activites.forEach(game => {
+            console.log(game);
+
+
+            const div = document.createElement("div");
+            const h3 = document.createElement("h3");
+            h3.textContent = game.nom;
+            const desc = document.createElement("p");
+            desc.textContent = game.description;
+            const image = document.createElement("img");
+            image.src = game["image-url"];
+
+
+            services.appendChild(div);
+            div.appendChild(h3);
+            div.appendChild(desc);
+            div.appendChild(image);
+        })
+
+
+        const div= document.getElementById("listeTemoignages");
+        // const temoin=document.getElementById("temoignages");
+
+        data.temoignages.forEach(element => {
+            console.log(element);
+
+
+            const h4 = document.createElement("h4");
+            h4.textContent = element.prenom;
+
+            // const experience= document.createElement("p");
+            // p.textContent= element.typeExperience;
+
+        
+            div.appendChild(h4);
+
+           
+        });
+
+
+    });
+
+
+
+
 
 
 
